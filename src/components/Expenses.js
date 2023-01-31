461cd99ea7ca756b49046a4d23dbfa847a480e01
mahimagupta24 <mahimagupta2401@gmail.com>
2023-01-31 21:02:52 +0530

    refactor

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,11 +3,17 @@ import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "./Card";
 import ExpenseFilter from "./ExpenseFilter";
+// import ExpenseDate from "./ExpenseDate";
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
+  
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses=props.items.filter(expense=>{
+    return expense.date.getFullYear().toString()===filteredYear
+  })
   return (
     <div>
       <Card className="expenses">
@@ -15,8 +21,11 @@ function Expenses(props) {
           selected={filteredYear}
           onFilterChange={filterChangeHandler}
         />
-        {props.items.map((expense) => (
+        {filteredExpenses.length===0&&<p>No expenses</p>}
+        {filteredExpenses.length>0&&
+        filteredExpenses.map((expense) => (
           <ExpenseItem
+          key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
